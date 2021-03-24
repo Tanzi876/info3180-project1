f627e6357b0b778d3d343e104e91b98bdc43e445
Tanzi876 <tanyaleelawrence@gmail.com>
2021-03-23 20:16:30 -0500

    Grid and viewproperties error but everything completed

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://project1:password@localhost/project1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://imhzviysnluuzo:517571e6fc8738ecf51f66dad56828bcbdf6ebb63ddca07a1a4b8c609d83252e@ec2-34-198-31-223.compute-1.amazonaws.com:5432/d1ebs7k7931um3''
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER= os.environ.get('UPLOAD_FOLDER') or 'uploads'
 class DevelopmentConfig(Config):
